21e3889cf518537f0b3335e5a551c01abb98a6f4
Rami Ahmed <108447976+Rami-Ahmed68@users.noreply.github.com>
2024-04-26 11:09:36 -0700

    Update showprofile.js

--- src/router/Users/showprofile.js
+++ src/router/Users/showprofile.js
@@ -35,7 +35,13 @@ router.get("/" , async ( req , res  , next ) => {
         }
 
         // getting the user by id
-        const user = await User.findById(Verify._id).populate("posts");
+        const user = await User.findById(Verify._id).populate([
+            path : "posts",
+            populate : {
+                path : "created_by",
+                select : "_id name avatar"
+            }
+        ]);
 
         // check if the user exists
         if (!user) {
