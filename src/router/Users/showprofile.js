4696ec553349d8c28fff84ec5f147a00ca077686
Rami Ahmed <108447976+Rami-Ahmed68@users.noreply.github.com>
2024-04-27 04:52:49 -0700

    Update showprofile.js

--- src/router/Users/showprofile.js
+++ src/router/Users/showprofile.js
@@ -52,8 +52,10 @@ router.get("/" , async ( req , res  , next ) => {
         // add like type if the like author id is equal the user id
             user.posts.forEach(post => {
                 post.likes.forEach(like => {
-                    if (like.liked_by == Verify._id) {
-                        like.like_type = {reaction_type : like.reaction_type , likeId : like._id}
+                    if (post.likes.length > 0 && like.liked_by == Verify._id) {
+                        post.like_type = {reaction_type : like.reaction_type , likeId : like.id}
+                    } else {
+                        post.like_type = ""
                     }
                  })
             });
