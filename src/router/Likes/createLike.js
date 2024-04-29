08395cdbeb448347d2a774cee4bd0ad241311e8d
Rami Ahmed <108447976+Rami-Ahmed68@users.noreply.github.com>
2024-04-29 04:35:34 -0700

    Update createLike.js

--- src/router/Likes/createLike.js
+++ src/router/Likes/createLike.js
@@ -47,7 +47,7 @@ router.post("/" , async ( req , res , next ) => {
         // get old like
         const oldLike = await Like.find({ liked_by : Verify._id })
 
-        return oldLike;
+        return res.send(oldLike);
         // if the user has already liked this post
         if (oldLike.length > 0) {
             return next(new ApiErrors("You Can't add more than one like" , 409));
